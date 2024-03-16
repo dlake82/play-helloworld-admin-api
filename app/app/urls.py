f9134d53c2702a19c078d9a02e3febf5e34d27a4
khw0828 <97077535+gusdn0828@users.noreply.github.com>
2024-03-16 16:43:59 +0900

    feat: 개발 환경 설정 완료

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import routers
+
 
 from users import views
 
